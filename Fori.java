258177f168d843f2026a00e6289a47c1373ba005
kalorak <kalotai92@gmail.com>
2025-02-13 15:32:35 +0100

    Adding some content v10

---
+++ Fori.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Fori {
+    public static void main(String[] args) {
+
+        Scanner sca = new Scanner(System.in);
+        int limit = sca.nextInt();
+        int days = sca.nextInt();
+
+        for (int i = 1; i <= limit; i++) {
+
+            System.out.println(limit);
+
+        }
+
+    }
+}
